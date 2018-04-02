596cf50f79fe329ab7e85bcdffed1bac935a2f28
Aleksey Krichevskiy <boolivar@gmail.com>
2018-04-02 20:49:26 +0300

    Yaml config

--- src/main/java/org/bool/lunch/Main.java
+++ src/main/java/org/bool/lunch/Main.java
@@ -1,70 +1,53 @@
 package org.bool.lunch;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.zookeeper.server.quorum.QuorumPeerMain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.esotericsoftware.yamlbeans.YamlWriter;
-
-import kafka.Kafka;
+import com.esotericsoftware.yamlbeans.YamlReader;
 
 public class Main {
+
+	private static final String CONFIG_YML = "/config.yml";
 	
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	
-	private static final Class<?>[] classesToRun = {
-		QuorumPeerMain.class,
-		Kafka.class
-	};
-	
-	private static final String[][] arguments = {
-		{"C:\\bin\\kafka_2.11-1.0.0\\config\\zookeeper.properties"},
-		{"C:\\bin\\kafka_2.11-1.0.0\\config\\server.properties"},
-	};
-	
     public static void main(String[] args) throws InterruptedException, IOException, ReflectiveOperationException {
-    	Lunch lunch = lunch();
-    	
-    	ExecutorService pool = Executors.newFixedThreadPool(lunch.getItems().size());
-    	
-    	for (LunchItem item : lunch.getItems()) {
+    	Lunch lunch = loadLunch(args.length > 0 ? args[0] : null);
+    	log.info("Lunch loaded: {}", lunch);
+    	run(lunch.getItems());
+    	log.info("Lunch started");
+    }
+    
+    private static void run(List<LunchItem> items) throws ClassNotFoundException {
+    	ExecutorService pool = Executors.newFixedThreadPool(items.size());
+    	for (LunchItem item : items) {
     		Luncher luncher = Luncher.create(item.getClassName(), item.getArgs());
     		pool.execute(luncher);
     		log.info("Started {} with arguments: {}", item.getClassName(), item.getArgs());
     	}
-    	
     	pool.shutdown();
     }
     
-    private static void writeConfiguration(String fileName, Object object) throws IOException {
-    	YamlWriter yamlWriter = new YamlWriter(new BufferedWriter(new FileWriter(fileName)));
-    	try {
-    		yamlWriter.write(object);
-    		log.info("Write configuration {} to file {}", object, fileName);
-    	} finally {
-    		yamlWriter.close();
+    private static Lunch loadLunch(String fileName) throws IOException {
+    	log.info("Read lunch from file: {}", fileName);
+    	try (InputStream in = openStream(fileName)) {
+    		YamlReader reader = new YamlReader(new InputStreamReader(in));
+    		return reader.read(Lunch.class);
     	}
     }
     
-    private static Lunch lunch() {
-    	Lunch lunch = new Lunch();
-    	List<LunchItem> items = new ArrayList<>(classesToRun.length);
-    	for (int i = 0; i < classesToRun.length; ++i) {
-    		LunchItem lunchItem = new LunchItem();
-    		lunchItem.setClassName(classesToRun[i].getName());
-    		lunchItem.setArgs(Arrays.asList(arguments[i]));
-    		items.add(lunchItem);
+    private static InputStream openStream(String fileName) throws IOException {
+    	if (fileName != null) {
+    		return new FileInputStream(fileName);
     	}
-    	lunch.setItems(items);
-    	return lunch;
+    	return Main.class.getResourceAsStream(CONFIG_YML);
     }
 }
