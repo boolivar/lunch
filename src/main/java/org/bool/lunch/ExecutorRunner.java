45d827dd8e0a2b972c8dd6cef0bd9e3c831fadd7
Aleksey Krichevskiy <boolivar@gmail.com>
2018-04-05 23:32:07 +0300

    ExecutorRunner extracted from ClassRunner

---
+++ src/main/java/org/bool/lunch/ExecutorRunner.java
@@ -0,0 +1,22 @@
+package org.bool.lunch;
+
+import java.util.Collection;
+import java.util.concurrent.Executor;
+
+public class ExecutorRunner implements Runner {
+
+	private final Executor executor;
+	
+	private final Runner runner;
+
+	public ExecutorRunner(Executor executor, Runner runner) {
+		this.executor = executor;
+		this.runner = runner;
+	}
+
+	@Override
+	public Process run(String command, Collection<String> args) {
+		executor.execute(() -> runner.run(command, args));
+		return null;
+	}
+}
