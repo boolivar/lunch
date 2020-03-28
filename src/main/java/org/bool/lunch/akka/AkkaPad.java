d52fad53154bd64dfd39c38cd08c7874a26525ba
Aleksey Krichevskiy <boolivar@gmail.com>
2020-03-28 23:31:05 +0300

    AkkaPad: create ActorSystem

--- src/main/java/org/bool/lunch/akka/AkkaPad.java
+++ src/main/java/org/bool/lunch/akka/AkkaPad.java
@@ -1,17 +1,21 @@
 package org.bool.lunch.akka;
 
+import org.bool.lunch.Command;
 import org.bool.lunch.Lunch;
-import org.bool.lunch.LunchBox;
+
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Behaviors;
 
 public class AkkaPad {
 	
-	private final LunchBox lunchBox;
+	private final LunchItemActorFactory lunchItemActorFactory;
 
-	public AkkaPad(LunchBox lunchBox) {
-		this.lunchBox = lunchBox;
+	public AkkaPad(LunchItemActorFactory lunchItemActorFactory) {
+		this.lunchItemActorFactory = lunchItemActorFactory;
 	}
 
 	public void launch(Lunch lunch) {
-		
+		ActorSystem<Command> actorSystem = ActorSystem
+				.create(Behaviors.setup(context -> new LunchActor(context, lunchItemActorFactory)), "Lunch");
 	}
 }
