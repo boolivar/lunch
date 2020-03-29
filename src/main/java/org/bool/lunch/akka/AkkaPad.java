870dea3667f352f36636448e50b54b4e9032a768
Aleksey Krichevskiy <boolivar@gmail.com>
2020-03-29 14:30:41 +0300

    AkkaPad implements LaunchPad

--- src/main/java/org/bool/lunch/akka/AkkaPad.java
+++ src/main/java/org/bool/lunch/akka/AkkaPad.java
@@ -1,12 +1,18 @@
 package org.bool.lunch.akka;
 
 import org.bool.lunch.Command;
+import org.bool.lunch.LaunchPad;
 import org.bool.lunch.Lunch;
+import org.bool.lunch.akka.LunchActor.LunchCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
 
-public class AkkaPad {
+public class AkkaPad implements LaunchPad {
+
+	private final Logger log = LoggerFactory.getLogger(AkkaPad.class);
 	
 	private final LunchItemActorFactory lunchItemActorFactory;
 
@@ -14,8 +20,11 @@ public class AkkaPad {
 		this.lunchItemActorFactory = lunchItemActorFactory;
 	}
 
+	@Override
 	public void launch(Lunch lunch) {
 		ActorSystem<Command> actorSystem = ActorSystem
 				.create(Behaviors.setup(context -> new LunchActor(context, lunchItemActorFactory)), "Lunch");
+		lunch.getItems().stream().map(LunchCommand::new).forEach(actorSystem::tell);
+		log.debug("Actor system tree: {}", actorSystem.printTree());
 	}
 }
