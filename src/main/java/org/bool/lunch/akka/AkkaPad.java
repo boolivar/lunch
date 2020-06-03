c8fec2d72be4ef7b57ef70acd455547531ebbdfb
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-03 20:04:05 +0300

    AkkaPad: join cluster

--- src/main/java/org/bool/lunch/akka/AkkaPad.java
+++ src/main/java/org/bool/lunch/akka/AkkaPad.java
@@ -8,11 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Adapter;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.cluster.Cluster;
+import akka.management.javadsl.AkkaManagement;
 
 public class AkkaPad implements LaunchPad {
 
-	private final Logger log = LoggerFactory.getLogger(AkkaPad.class);
+	private static final Logger log = LoggerFactory.getLogger(AkkaPad.class);
 	
 	private final LunchItemActorFactory lunchItemActorFactory;
 
@@ -24,6 +27,14 @@ public class AkkaPad implements LaunchPad {
 	public void launch(Lunch lunch) {
 		ActorSystem<Command> actorSystem = ActorSystem
 				.create(Behaviors.setup(context -> new LunchActor(context, lunchItemActorFactory)), "Lunch");
+		
+		akka.actor.ActorSystem classic = Adapter.toClassic(actorSystem);
+		
+		AkkaManagement.get(classic)
+				.start();
+		Cluster cluster = Cluster.get(classic);
+		cluster.join(cluster.selfAddress());
+		
 		lunch.getItems().stream().map(LunchCommand::new).forEach(actorSystem::tell);
 		log.debug("Actor system tree: {}", actorSystem.printTree());
 	}
