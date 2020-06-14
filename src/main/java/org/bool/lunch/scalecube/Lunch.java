2f9eeae556fb161dc9b9a1aa7ef325c0ad387b6a
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-15 00:44:27 +0300

    Lunch gateway

--- src/main/java/org/bool/lunch/scalecube/Lunch.java
+++ src/main/java/org/bool/lunch/scalecube/Lunch.java
@@ -5,14 +5,20 @@ import org.bool.lunch.DefaultRunnerFactory;
 import org.bool.lunch.LunchRunner;
 import org.bool.lunch.PidReader;
 import org.bool.lunch.RunnerType;
+import org.bool.lunch.scalecube.gateway.LunchHttpGateway;
+import org.bool.lunch.scalecube.gateway.LunchHttpServer;
+import org.bool.lunch.scalecube.gateway.ServiceCallHttpHandler;
 
 import java.util.concurrent.Executors;
 
+import io.scalecube.net.Address;
 import io.scalecube.services.Microservices;
 import io.scalecube.services.ServiceEndpoint;
 import io.scalecube.services.ServiceInfo;
 import io.scalecube.services.discovery.ScalecubeServiceDiscovery;
 import io.scalecube.services.discovery.api.ServiceDiscovery;
+import io.scalecube.services.gateway.Gateway;
+import io.scalecube.services.gateway.GatewayOptions;
 
 public class Lunch {
 
@@ -27,12 +33,20 @@ public class Lunch {
 				.services(ServiceInfo.fromServiceInstance(service)
 						.tag("role", "seed")
 						.build())
+				.gateway(this::gateway)
 				.discovery(this::discovery)
 				.startAwait();
 		
 		synchronized (microservices) {
 			microservices.wait();
 		}
+	}
+	
+	private Gateway gateway(GatewayOptions options) {
+		Address address = Address.create("localhost", options.port());
+		ServiceCallHttpHandler handler = new ServiceCallHttpHandler(options.call());
+		LunchHttpServer server = new LunchHttpServer("localhost", options.port(), handler);
+		return new LunchHttpGateway(options.id(), address, server.bind().cache());
 	}
 		
 	private ServiceDiscovery discovery(ServiceEndpoint endpoint) {
