ef6be8cf513fe8194230dcc54379a33ccb3b9316
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-15 00:42:51 +0300

    LunchHttpGateway

---
+++ src/main/java/org/bool/lunch/scalecube/gateway/LunchHttpGateway.java
@@ -0,0 +1,41 @@
+package org.bool.lunch.scalecube.gateway;
+
+import io.scalecube.net.Address;
+import io.scalecube.services.gateway.Gateway;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+
+public class LunchHttpGateway implements Gateway {
+
+	private final String id;
+	
+	private final Address address;
+	
+	private final Mono<? extends DisposableServer> server;
+	
+	public LunchHttpGateway(String id, Address address, Mono<? extends DisposableServer> server) {
+		this.id = id;
+		this.address = address;
+		this.server = server;
+	}
+
+	@Override
+	public String id() {
+		return id;
+	}
+
+	@Override
+	public Address address() {
+		return address;
+	}
+
+	@Override
+	public Mono<Gateway> start() {
+		return server.thenReturn(this);
+	}
+
+	@Override
+	public Mono<Void> stop() {
+		return server.doOnNext(DisposableServer::dispose).then();
+	}
+}
---
+++ src/test/java/org/bool/lunch/scalecube/LunchHttpGatewayTest.java
@@ -0,0 +1,46 @@
+package org.bool.lunch.scalecube;
+
+import org.bool.lunch.scalecube.gateway.LunchHttpGateway;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+import io.scalecube.net.Address;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+
+class LunchHttpGatewayTest {
+	
+	DisposableServer server = mock(DisposableServer.class);
+
+	LunchHttpGateway gateway = new LunchHttpGateway("test-gateway", Address.from("test:1234"), Mono.just(server));
+
+	@Test
+	void testNoInteractions() {
+		assertEquals("test-gateway", gateway.id());
+		assertEquals("test", gateway.address().host());
+		assertEquals(1234, gateway.address().port());
+		
+		gateway.start();
+		gateway.stop();
+		
+		then(server)
+				.shouldHaveNoInteractions();
+	}
+	
+	@Test
+	void testStart() {
+		assertSame(gateway, gateway.start().block());
+	}
+	
+	@Test
+	void testStop() {
+		gateway.stop().block();
+		
+		then(server)
+				.should().dispose();
+	}
+}
