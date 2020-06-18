a0b86962a28abd66a4e43b5c370574d2da2c7529
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-18 19:47:34 +0300

    LunchService: land(), stats()

--- src/main/java/org/bool/lunch/scalecube/LocalLunchService.java
+++ src/main/java/org/bool/lunch/scalecube/LocalLunchService.java
@@ -4,10 +4,16 @@ import org.bool.lunch.LunchItem;
 import org.bool.lunch.LunchRunner;
 import org.bool.lunch.Lunched;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.ReplayProcessor;
 
 public class LocalLunchService implements LunchService {
@@ -15,15 +21,23 @@ public class LocalLunchService implements LunchService {
     private final LunchRunner lunchRunner;
     
     private final Executor executor;
-
+    
+    private final Map<String, Lunched> lunchedMap;
+    
     public LocalLunchService(LunchRunner lunchRunner, Executor executor) {
+        this(lunchRunner, executor, new ConcurrentHashMap<>());
+    }
+
+    public LocalLunchService(LunchRunner lunchRunner, Executor executor, Map<String, Lunched> lunchedMap) {
         this.lunchRunner = lunchRunner;
         this.executor = executor;
+        this.lunchedMap = lunchedMap;
     }
 
     @Override
     public Flux<Lunched> launch(LunchItem item) {
         ReplayProcessor<Lunched> result = ReplayProcessor.cacheLast();
+        result.subscribe(lunch -> lunchedMap.put(lunch.getPid(), lunch));
         executor.execute(() -> launch(item, result.sink()));
         return result;
     }
@@ -31,6 +45,9 @@ public class LocalLunchService implements LunchService {
     private void launch(LunchItem item, FluxSink<Lunched> sink) {
         try {
             Lunched lunch = lunchRunner.launch(item);
+            
+            lunchedMap.put(lunch.getPid(), lunch);
+            
             sink.next(new Lunched(lunch.getPid(), null, lunch.getLunchItem()));
             
             int exitCode = lunch.getProcess().waitFor();
@@ -41,5 +58,27 @@ public class LocalLunchService implements LunchService {
         } catch (Exception e) {
             sink.error(e);
         }
+    }
+    
+    @Override
+    public Mono<Void> land(String pid) {
+        return Mono.just(pid)
+                .map(lunchedMap::get)
+                .filter(Objects::nonNull)
+                .map(Lunched::getProcess)
+                .doOnNext(Process::destroy)
+                .single()
+                .then();
+    }
+
+    @Override
+    public Mono<List<Stat>> stats() {
+        return Mono.just(lunchedMap.values())
+                .map(values -> values.stream().map(this::buildStat).collect(Collectors.toList()))
+                .cache();
+    }
+    
+    private Stat buildStat(Lunched lunched) {
+        return new Stat(lunched.getPid(), lunched.getLunchItem(), lunched.getProcess().isAlive() ? null : lunched.getProcess().exitValue());
     }
 }
--- src/main/java/org/bool/lunch/scalecube/LunchService.java
+++ src/main/java/org/bool/lunch/scalecube/LunchService.java
@@ -3,12 +3,22 @@ package org.bool.lunch.scalecube;
 import org.bool.lunch.LunchItem;
 import org.bool.lunch.Lunched;
 
+import java.util.List;
+
 import io.scalecube.services.annotations.Service;
 import io.scalecube.services.annotations.ServiceMethod;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public interface LunchService {
+    
     @ServiceMethod
     Flux<Lunched> launch(LunchItem item);
+    
+    @ServiceMethod
+    Mono<Void> land(String pid);
+    
+    @ServiceMethod
+    Mono<List<Stat>> stats();
 }
