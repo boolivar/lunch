63cf75e9af5a6aa05a7bcddb3f0fb1295a6833eb
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-22 14:24:42 +0300

    Lunch: server port

--- src/main/java/org/bool/lunch/scalecube/Lunch.java
+++ src/main/java/org/bool/lunch/scalecube/Lunch.java
@@ -6,8 +6,6 @@ import org.bool.lunch.LunchRunner;
 import org.bool.lunch.PidReader;
 import org.bool.lunch.RunnerType;
 
-import java.util.concurrent.Executors;
-
 import io.scalecube.services.Microservices;
 import io.scalecube.services.ServiceEndpoint;
 import io.scalecube.services.ServiceInfo;
@@ -23,8 +21,15 @@ public class Lunch {
 
 	private final LunchService service;
 	
+	private final int port;
+	
 	public Lunch(LunchService service) {
+		this(service, 0);
+	}
+	
+	public Lunch(LunchService service, int port) {
 		this.service = service;
+		this.port = port;
 	}
 
 	public Mono<Microservices> launch() {
@@ -38,19 +43,19 @@ public class Lunch {
 	}
 	
 	private Gateway gateway(GatewayOptions options) {
-	    return new HttpGateway(options);
+	    return new HttpGateway(options.port(port));
 	}
 		
 	private ServiceDiscovery discovery(ServiceEndpoint endpoint) {
 		return new ScalecubeServiceDiscovery(endpoint);
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		CachedRunnerFactory<String> factory = new CachedRunnerFactory<>(new DefaultRunnerFactory(), RunnerType::valueOf);
 		LunchRunner runner = new LunchRunner(factory::lookup, PidReader.DEFAULT);
 		Luncher luncher = new Luncher(runner, Schedulers.newElastic("local-lunch"));
 		LocalLunchService lunchService = new LocalLunchService(luncher);
-		Lunch lunch = new Lunch(lunchService);
+		Lunch lunch = new Lunch(lunchService, args.length > 0 ? Integer.parseInt(args[0]) : 0);
 		lunch.launch()
 				.flatMap(Microservices::onShutdown).block();
 	}
