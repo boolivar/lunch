961e4a502ceb29b2b404d1827b47dce70f8aa5ce
Aleksey Krichevskiy <boolivar@gmail.com>
2020-06-27 22:41:56 +0300

    LunchMessageProcessor: scale header handling

--- src/main/java/org/bool/lunch/scalecube/gateway/LunchMessageProcessor.java
+++ src/main/java/org/bool/lunch/scalecube/gateway/LunchMessageProcessor.java
@@ -1,8 +1,11 @@
 package org.bool.lunch.scalecube.gateway;
 
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import io.scalecube.services.ServiceCall;
+import io.scalecube.services.ServiceReference;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.registry.api.ServiceRegistry;
 import reactor.core.publisher.Mono;
@@ -20,6 +23,38 @@ public class LunchMessageProcessor implements Function<ServiceMessage, Mono<Serv
 	
 	@Override
 	public Mono<ServiceMessage> apply(ServiceMessage message) {
-		return serviceCall.requestOne(message);
+		String scale = message.header("scale");
+		
+		if (scale == null) {
+			return serviceCall.requestOne(message);
+		}
+		
+		return Mono.just(scale)
+				.map(Integer::valueOf).onErrorReturn(Integer.MAX_VALUE)
+				.flatMap(s -> requestMany(message, s));
+	}
+	
+	private Mono<ServiceMessage> requestMany(ServiceMessage message, Integer scale) {
+		List<ServiceReference> refs = serviceRegistry.lookupService(message);
+
+		if (refs.isEmpty()) {
+			return Mono.empty();
+		}
+		
+		List<Mono<ServiceMessage>> monos = refs.stream()
+				.limit(scale)
+				.map(ref -> requestOne(ref, message))
+				.collect(Collectors.toList());
+		
+		return Mono.zip(monos, this::buildMessage);
+	}
+	
+	private Mono<ServiceMessage> requestOne(ServiceReference ref, ServiceMessage message) {
+		return serviceCall.requestOne(message, null, ref.address())
+				.onErrorResume(t -> Mono.just(ServiceMessage.builder().data(t.getMessage()).build()));
+	}
+	
+	private ServiceMessage buildMessage(Object[] data) {
+		return ServiceMessage.builder().data(data).build();
 	}
 }
