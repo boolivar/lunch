7f9ab4ebc0c868ab29c341387ebad5a9b699480d
Aleksey Krichevskiy <boolivar@gmail.com>
2020-07-06 22:41:42 +0300

    LunchProcess interface, NativeLunchProcess implementation

---
+++ src/main/java/org/bool/lunch/LunchProcess.java
@@ -0,0 +1,30 @@
+package org.bool.lunch;
+
+import java.time.Duration;
+
+public interface LunchProcess {
+
+	Duration INFINITE_WAIT = null;
+
+	default boolean isAlive() {
+		return exitCode() == null;
+	}
+	
+	default Integer exitCode() {
+		try {
+			return waitFor(Duration.ZERO);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Unexpected exception", e);
+		}
+	}
+	
+	default Integer waitFor() throws InterruptedException {
+		return waitFor(INFINITE_WAIT);
+	}
+	
+	String getId();
+	
+	Integer waitFor(Duration duration) throws InterruptedException;
+	
+	void destroy();
+}
---
+++ src/main/java/org/bool/lunch/NativeLunchProcess.java
@@ -0,0 +1,36 @@
+package org.bool.lunch;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class NativeLunchProcess implements LunchProcess {
+
+	private final String pid;
+	
+	private final Process process;
+	
+	public NativeLunchProcess(String pid, Process process) {
+		this.pid = pid;
+		this.process = process;
+	}
+	
+	@Override
+	public String getId() {
+		return pid;
+	}
+
+	@Override
+	public Integer waitFor(Duration duration) throws InterruptedException {
+		if (duration == INFINITE_WAIT) {
+			return process.waitFor();
+		}
+		return process.waitFor(duration.toMillis(), TimeUnit.MILLISECONDS)
+				? process.exitValue()
+				: null;
+	}
+
+	@Override
+	public void destroy() {
+		process.destroy();
+	}
+}
