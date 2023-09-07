f33e5f4eb6fdc78dbf577c89974d90a25b6c7aaf
Aleksey Krichevskiy <boolivar@gmail.com>
2023-09-08 01:44:39 +0300

    Main: Files.newBufferedReader (system encoding) usage

--- src/main/java/org/bool/lunch/Main.java
+++ src/main/java/org/bool/lunch/Main.java
@@ -1,24 +1,24 @@
 package org.bool.lunch;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
-
-import org.bool.lunch.akka.AkkaPad;
-import org.bool.lunch.akka.LunchItemActorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
+import org.bool.lunch.akka.AkkaPad;
+import org.bool.lunch.akka.LunchItemActorFactory;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
-	private static final String CONFIG_YML = "/config.yml";
-	
+	private static final String CONFIG_YML = "config.yml";
+
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
-	
+
 	public static void main(String[] args) throws InterruptedException, IOException, ReflectiveOperationException {
 		Lunch lunch = loadLunch(args.length > 0 ? args[0] : null);
 		log.info("Lunch loaded: {}", lunch);
@@ -35,17 +35,10 @@ public class Main {
 	}
 	
 	private static Lunch loadLunch(String fileName) throws IOException {
-		log.info("Read lunch from file: {}", fileName);
-		try (InputStream in = openStream(fileName)) {
-			YamlReader reader = new YamlReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-			return reader.read(Lunch.class);
+		Path configPath = Paths.get(fileName != null ? fileName : CONFIG_YML);
+		log.info("Read lunch from file: {}", configPath);
+		try (Reader reader = Files.newBufferedReader(configPath)) {
+			return new YamlReader(reader).read(Lunch.class);
 		}
-	}
-	
-	private static InputStream openStream(String fileName) throws IOException {
-		if (fileName != null) {
-			return new FileInputStream(fileName);
-		}
-		return Main.class.getResourceAsStream(CONFIG_YML);
 	}
 }
