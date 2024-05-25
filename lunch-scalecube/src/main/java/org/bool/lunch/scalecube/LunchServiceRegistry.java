01de96d9cec945ad81bc81fb6f2629c123c45c45
Aleksey Krichevskiy <boolivar@gmail.com>
2024-05-25 22:29:01 +0300

    refactor: map based LunchServiceRegistry

--- lunch-scalecube/src/main/java/org/bool/lunch/scalecube/LunchServiceRegistry.java
+++ lunch-scalecube/src/main/java/org/bool/lunch/scalecube/LunchServiceRegistry.java
@@ -1,33 +1,30 @@
 package org.bool.lunch.scalecube;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import io.scalecube.services.ServiceEndpoint;
 import io.scalecube.services.ServiceReference;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.registry.api.ServiceRegistry;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class LunchServiceRegistry implements ServiceRegistry {
 
 	private final ServiceFilter serviceFilter;
-	
-	private final List<ServiceEndpoint> endpoints;
-	
+
+	private final Map<String, ServiceEndpoint> endpoints;
+
 	public LunchServiceRegistry(ServiceFilter serviceFilter) {
-		this(serviceFilter, new CopyOnWriteArrayList<>());
+		this(serviceFilter, new ConcurrentHashMap<>());
 	}
-	
-	public LunchServiceRegistry(ServiceFilter serviceFilter, List<ServiceEndpoint> endpoints) {
-		this.serviceFilter = serviceFilter;
-		this.endpoints = endpoints;
-	}
-	
+
 	@Override
 	public List<ServiceEndpoint> listServiceEndpoints() {
-		return endpoints;
+		return List.copyOf(endpoints.values());
 	}
 
 	@Override
@@ -39,23 +36,21 @@ public class LunchServiceRegistry implements ServiceRegistry {
 	public List<ServiceReference> lookupService(ServiceMessage request) {
 		return findServiceReferences(ref -> serviceFilter.accept(ref, request));
 	}
-	
+
 	private List<ServiceReference> findServiceReferences(Predicate<ServiceReference> filter) {
-		return endpoints.stream()
+		return endpoints.values().stream()
 				.flatMap(e -> e.serviceReferences().stream())
 				.filter(filter)
-				.collect(Collectors.toList())
-				;
+				.toList();
 	}
 	
 	@Override
 	public boolean registerService(ServiceEndpoint serviceEndpoint) {
-		return endpoints.add(serviceEndpoint);
+		return endpoints.putIfAbsent(serviceEndpoint.id(),  serviceEndpoint) == null;
 	}
 
 	@Override
 	public ServiceEndpoint unregisterService(String endpointId) {
-		endpoints.removeIf(e -> endpointId.equals(e.id()));
-		return null;
+		return endpoints.remove(endpointId);
 	}
 }
