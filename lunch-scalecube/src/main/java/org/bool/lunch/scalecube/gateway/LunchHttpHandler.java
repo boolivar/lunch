6c9fa3d2e5d60d8d9cb72a183d67685628cc6dc2
Aleksey Krichevskiy <boolivar@gmail.com>
2024-05-26 23:34:09 +0300

    fix: LunchHttpHandler retain buffers

--- lunch-scalecube/src/main/java/org/bool/lunch/scalecube/gateway/LunchHttpHandler.java
+++ lunch-scalecube/src/main/java/org/bool/lunch/scalecube/gateway/LunchHttpHandler.java
@@ -13,14 +13,15 @@ import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.Unpooled;
 import io.scalecube.services.api.ServiceMessage;
 import io.scalecube.services.transport.api.DataCodec;
+import lombok.SneakyThrows;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 
 public class LunchHttpHandler implements BiFunction<HttpServerRequest, HttpServerResponse, Publisher<Void>> {
-	
+
 	private final Function<ServiceMessage, Mono<ServiceMessage>> messageProcessor;
-	
+
 	private final DataCodec dataCodec;
 
 	public LunchHttpHandler(Function<ServiceMessage, Mono<ServiceMessage>> messageProcessor, DataCodec dataCodec) {
@@ -31,39 +32,39 @@ public class LunchHttpHandler implements BiFunction<HttpServerRequest, HttpServe
 	@Override
 	public Publisher<Void> apply(HttpServerRequest request, HttpServerResponse response) {
 		return request.receive().aggregate()
-				.defaultIfEmpty(Unpooled.EMPTY_BUFFER)
-				.map(buf -> createMessage(request, buf))
-				.flatMap(messageProcessor)
-				.flatMap(message -> encodeResponse(response, message))
-				.transform(response::sendObject)
-				;
+			.defaultIfEmpty(Unpooled.EMPTY_BUFFER)
+			.map(buf -> createMessage(request, buf))
+			.flatMap(messageProcessor)
+			.flatMap(message -> sendResponse(response, message))
+			;
 	}
 
 	private ServiceMessage createMessage(HttpServerRequest request, ByteBuf buf) {
-		var headers = Stream.concat(request.params() != null ? request.params().entrySet().stream() : Stream.of(),
-				request.requestHeaders().entries().stream())
-			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b));
+		var headers = Stream.concat(
+				request.params() != null ? request.params().entrySet().stream() : Stream.of(),
+				request.requestHeaders().entries().stream()
+			).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b));
 		return ServiceMessage.builder()
-				.qualifier(request.path())
-				.headers(headers)
-				.data(buf)
-				.build();
+			.qualifier(request.path())
+			.headers(headers)
+			.data(buf.retain())
+			.build();
 	}
 
-	private Mono<ByteBuf> encodeResponse(HttpServerResponse response, ServiceMessage message) {
+	@SneakyThrows
+	private Mono<Void> sendResponse(HttpServerResponse response, ServiceMessage message) {
 		if (!message.hasData()) {
-			return Mono.empty();
+			return response.send();
 		}
 		if (message.hasData(ByteBuf.class)) {
-			return Mono.just(message.data());
+			return response.sendObject((ByteBuf) message.data()).then();
 		}
 		ByteBuf buffer = response.alloc().buffer();
 		try {
 			dataCodec.encode(new ByteBufOutputStream(buffer), message.data());
-		} catch (Exception e) {
+			return response.sendObject(buffer.retain()).then();
+		} finally {
 			buffer.release();
-			throw new RuntimeException("Error encode data from message: " + message, e);
 		}
-		return Mono.just(buffer);
 	}
 }
